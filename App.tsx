2ba2214a60ce7627131f6f99a1e744e002e25861
machamp0714 <rateliasudaisuki@gmail.com>
2020-11-29 23:12:13 +0900

    firebaseと連携

--- App.tsx
+++ App.tsx
@@ -1,12 +1,43 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import * as firebase from 'firebase';
+import 'firebase/firestore';
+
+import { firebaseConfig } from './firebase-config';
+
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+}
+
+type Shop = {
+  name: string;
+  place: string;
+};
 
 export default function App() {
+  const [shops, setShops] = useState<Shop[]>([]);
+
+  useEffect(() => {
+    getFirebaseItems();
+  }, []);
+
+  const getFirebaseItems = async () => {
+    const snap = await firebase.firestore().collection('shops').get();
+    const shops = snap.docs.map(doc => doc.data() as Shop);
+    setShops(shops);
+  }
+
+  const shopItems = shops.map((shop, index) => (
+    <View style={{ margin: 10 }} key={index.toString()}>
+      <Text>{shop.name}</Text>
+      <Text>{shop.place}</Text>
+    </View>
+  ));
+
   return (
     <View style={styles.container}>
-      <Text>Hello!</Text>
-      <StatusBar style="auto" />
+      {shopItems}
     </View>
   );
 }
