1b05147011adb8be5633770b88ce6f90e36249a9
machamp0714 <rateliasudaisuki@gmail.com>
2020-12-03 00:11:34 +0900

    TabにIconを追加

--- src/navigation/MainTabNavigator.tsx
+++ src/navigation/MainTabNavigator.tsx
@@ -1,5 +1,6 @@
 import React, { FC } from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Feather } from '@expo/vector-icons';
 
 import HomeStackNavigator from './HomeStackNavigator';
 import UserScreen from '../screens/UserScreen';
@@ -8,9 +9,32 @@ const Tab = createBottomTabNavigator();
 
 const MainTabNavigator: FC = () => {
   return (
-    <Tab.Navigator>
-      <Tab.Screen name='Home' component={HomeStackNavigator} />
-      <Tab.Screen name='User' component={UserScreen} />
+    <Tab.Navigator
+      tabBarOptions={{
+        activeTintColor: '#900',
+        inactiveTintColor: '#999'
+      }}
+    >
+      <Tab.Screen
+        name='Home'
+        component={HomeStackNavigator}
+        options={{
+          tabBarLabel: 'Home',
+          tabBarIcon: ({ color, size }) => (
+            <Feather name='home' color={color} size={size} />
+          )
+        }}
+      />
+      <Tab.Screen
+        name='User'
+        component={UserScreen}
+        options={{
+          tabBarLabel: 'User',
+          tabBarIcon: ({ color, size }) => (
+            <Feather name='user' color={color} size={size} />
+          )
+        }}
+      />
     </Tab.Navigator>
   )
 }
