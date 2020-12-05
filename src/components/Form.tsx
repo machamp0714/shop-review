4d918f98e8690051558d50bb12d718db5bb79a0f
machamp0714 <rateliasudaisuki@gmail.com>
2020-12-05 21:46:59 +0900

    create Form component

---
+++ src/components/Form.tsx
@@ -0,0 +1,37 @@
+import React, { FC } from 'react';
+import { StyleSheet, View, TextInput, Text } from 'react-native';
+
+type Props = {
+  onChangeText: (text: string) => void;
+  value: string;
+  label: string;
+};
+
+const Form: FC<Props> = ({ onChangeText, value, label}) => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.label}>{label}</Text>
+      <TextInput
+        style={styles.input}
+        onChangeText={(text) => onChangeText(text)}
+        value={value}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 16
+  },
+  input: {
+    height: 40,
+    borderColor: '#999',
+    borderBottomWidth: 1,
+  },
+  label: {
+    fontWeight: 'bold'
+  }
+});
+
+export default Form;
