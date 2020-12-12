11f4d41db85d1bf9da8d6093175cec2e708e5a74
machamp0714 <rateliasudaisuki@gmail.com>
2020-12-12 15:31:04 +0900

    create ReviewsContext

--- App.tsx
+++ App.tsx
@@ -1,14 +1,19 @@
 import React, { useState }  from 'react';
 import AppNavigator from './src/navigation/AppNavigator';
 import { UserContext } from './src/contexts/userContexts';
+import { ReviewsContext } from './src/contexts/reviewsContext';
 import { User } from './src/services/models/user';
+import { Review } from './src/services/models/review';
 
 export default function App() {
   const [user, setUser] = useState<User | null>(null);
+  const [reviews, setReviews] = useState<Review[]>([]);
 
   return (
     <UserContext.Provider value={{ user, setUser }} >
-      <AppNavigator />
+      <ReviewsContext.Provider value={{ reviews, setReviews }}>
+        <AppNavigator />
+      </ReviewsContext.Provider>
     </UserContext.Provider>
   );
 }
